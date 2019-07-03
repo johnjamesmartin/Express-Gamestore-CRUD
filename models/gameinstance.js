50eafd5dfa568272464f7f4f7c9fb8b5319bdc20
John Martin <johnjamesmartin@live.com>
2019-07-04 00:54:20 +0100

    Populate gameinstance model

---
+++ models/gameinstance.js
@@ -0,0 +1,21 @@
+/* Dependencies
+ *****************************************/
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+/* Schema
+ *****************************************/
+const GameInstanceSchema = new Schema({
+  game: { type: Schema.Types.ObjectId, ref: 'Game', required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  numberInStock: { type: Number, required: true }
+});
+
+/* Virtuals:
+ *****************************************/
+GameInstanceSchema.virtual('url').get(function() {
+  return '/catalog/gameinstance/' + this._id;
+});
+
+module.exports = mongoose.model('GameInstance', GameInstanceSchema);
